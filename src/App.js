d9bc5a031d98a1c8091de01d07ea800490f87635
adriano.souza <adriano.souza@zpesystems.com>
2022-10-16 20:52:31 -0300

    Testing Navbar Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Header from './components/Header'
+import React from 'react'
+import Navbar from './components/Navbar'
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header text="Magbank é incrível!" color="#F00"/>
+      <Navbar />
     </div>
   );
 }
--- src/components/Header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Header = ({ text, color }) => (<h1 style={{ color }}>{ text }</h1>);
-
-export default Header;
