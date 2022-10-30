34adf06adee6d616f16e4c9d27125b6a2673af73
adriano.souza <adriano.souza@zpesystems.com>
2022-10-30 17:28:16 -0300

    feat(faq): Component creation and styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,47 +1,46 @@
 import React from 'react';
-import {Container,
-  Navbar, 
+import {
+  Container,
+  Navbar,
   Nav,
-  Button,
   ButtonGroup,
-  NavDropdown
+  Button,
+  NavDropdown,
 } from 'react-bootstrap';
 import './index.scss';
 import logo from '../../assets/logo.svg';
 
 const Navigation = () => (
-  <Navbar expand="lg" variant="dark">
+  <Navbar variant='dark' expand='lg'>
     <Container>
-      <Navbar.Brand href="#home">
+      <Navbar.Brand href='#home'>
         <img
           src={logo}
-          height="30"
-          className="d-inline-block align-top"
-          alt="Magbank logo"
+          height='30'
+          className='d-inline-block align-top'
+          alt='Magbank logo'
         />
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#cartao">Cartão</Nav.Link>
-          <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
-          <Nav.Link href="#faq">FAQ</Nav.Link>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='me-auto'>
+          <Nav.Link href='#cartao'>Cartão</Nav.Link>
+          <Nav.Link href='#quemsomos'>Quem Somos</Nav.Link>
+          <Nav.Link href='#faq'>FAQ</Nav.Link>
         </Nav>
-        <ButtonGroup aria-label="Basic example">
-          <Button variant="outline-light">
-            <NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+        <ButtonGroup aria-label='Basic example'>
+          <Button variant='outline-light'>
+            <NavDropdown title='Acessar minha conta' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>
+                Pessoa Física
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href='#action/3.2'>
+                Pessoa Jurídica
               </NavDropdown.Item>
             </NavDropdown>
           </Button>
-          <Button variant="outline-light">abra sua conta</Button>
+          <Button variant='outline-light'>abra sua conta</Button>
         </ButtonGroup>
       </Navbar.Collapse>
     </Container>
